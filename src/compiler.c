2e2e3b9afcea1dce06ad00f48f18a936e0cd22e8
mykeduong <hongminh4402@gmail.com>
2023-12-16 20:56:26 -0700

    Parsing Prefix Expressions

--- src/compiler.c
+++ src/compiler.c
@@ -1,10 +1,37 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 
 #include "bytechunk.h"
 #include "compiler.h"
 #include "common.h"
 #include "scanner.h"
+
+typedef struct {
+  Token current;
+  Token previous;
+  bool hadError;
+  bool panicMode;
+} Parser;
+
+typedef enum {
+  PREC_NONE,
+  PREC_ASSIGNMENT, // =
+  PREC_OR,         // or
+  PREC_AND,        // and 
+  PREC_EQUALITY,   // == !=
+  PREC_COMPARISON, // < > <= >= 
+  PREC_TERM,       // + -
+  PREC_FACTOR,     // * /
+  PREC_UNARY,      // ! -
+  PREC_CALL,       // . () 
+  PREC_PRIMARY
+} Precedence;
+
+static ByteChunk* currentChunk(ByteChunk* compilingChunk) {
+  return compilingChunk;
+}
 
 static void errorAt(Parser* parser, Token* token, const char* message) {
   if (parser->panicMode) return;
@@ -50,18 +77,90 @@ static void consume(Parser* parser, Scanner* scanner, TokenType type, const char
   errorAtCurrent(parser, message);
 }
 
+static void emitByte(Parser* parser, ByteChunk* compilingChunk, uint8_t byte) {
+  writeChunk(currentChunk(compilingChunk), byte, parser->previous.line);
+}
+
+static void emitBytes(Parser* parser, ByteChunk* compilingChunk, uint8_t byte1, uint8_t byte2) {
+  emitByte(parser, compilingChunk, byte1);
+  emitByte(parser, compilingChunk, byte2);
+}
+
+static void emitReturn(Parser* parser, ByteChunk* compilingChunk) {
+  emitByte(parser, compilingChunk, OP_RETURN);
+}
+
+static void emitConstant(Parser* parser, ByteChunk* compilingChunk, Value value) {
+  writeConstant(currentChunk(compilingChunk), value, parser->previous.line);
+}
+
+static void endCompiler(Parser* parser, ByteChunk* compilingChunk) {
+  emitReturn(parser, compilingChunk);
+}
+
+static void parsePrecedence(Precedence precedence) {
+  
+}
+
+static void binary(Parser* parser, Scanner* scanner, ByteChunk* compilingChunk) {
+  TokenType operatorType = parser.previous.type;
+  ParserRule* rule = getRule(operatorType);
+  parsePrecedence((Precedence) rule->precedence + 1);
+
+  switch (operatorType) {
+    case TOKEN_PLUS:       emitByte(parser, compilingChunk, OP_ADD); break;
+    case TOKEN_MINUS:      emitByte(parser, compilingChunk, OP_SUBTRACT); break;
+    case TOKEN_STAR:       emitByte(parser, compilingChunk, OP_MULTIPLY); break;
+    case TOKEN_SLASH:      emitByte(parser, compilingChunk, OP_DIVIDE); break;
+    default: return; // unreachable
+  }
+}
+
+static void grouping(Parser* parser, Scanner* scanner) {
+  expression();
+  consume(parser, scanner, TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+
+static void number(Parser* parser, Scanner* scanner, ByteChunk* compilingChunk) {
+  double value = strtod(parser->previous.start, NULL);
+  emitConstant(parser, compilingChunk, value);
+} 
+
+static void unary(Parser* parser, Scanner* scanner, ByteChunk* compilingChunk) {
+  TokenType operatorType = parser->previous.type;
+
+  // Compile the operand;
+  parsePrecedence(PREC_UNARY);
+
+  // Emit the operator instruction.
+  switch (operatorType) {
+    case TOKEN_MINUS: emitByte(parser, compilingChunk, OP_NEGATE); break;
+    default: return;
+  }
+}
+
+static void expression() {
+  parsePrecedence(PREC_ASSIGNMENT);
+}
+
+
+
+
 bool compile(const char *source, ByteChunk* chunk) {
   Scanner scanner;
   initScanner(&scanner, source);
 
   Parser parser;
-
   parser.hadError = false;
   parser.panicMode = false;
+  
+  ByteChunk* compilingChunk = chunk;
 
   advance(&parser, &scanner);
-  expression(scanner);
+  expression();
   consume(&parser, &scanner, TOKEN_EOF, "Expect end of expression.");
+  endCompiler(&parser, compilingChunk);
+
   return !parser.hadError;
 }
 
--- src/compiler.h
+++ src/compiler.h
@@ -5,13 +5,6 @@
 #include "scanner.h"
 #include "common.h"
 
-typedef struct {
-  Token current;
-  Token previous;
-  bool hadError;
-  bool panicMode;
-} Parser;
-
 bool compile(const char* source, ByteChunk* chunk);
 
 #endif 
